565bc6c6c38ddaaaabdbaf959be991c56eaca833
salilsrivastava <salil.769@gmail.com>
2024-04-30 00:55:04 +0530

    Added endpoint to convert epoch time

--- src/main/java/org/bits/devops/Main.java
+++ src/main/java/org/bits/devops/Main.java
@@ -1,22 +1,138 @@
 package org.bits.devops;
 
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
-        long epochTime = 1714413944;
-        String dateTime = convertEpochToDateTime(epochTime);
-        System.out.println("Epoch Time: " + epochTime);
-        System.out.println("Date and Time: " + dateTime);
+    private static HttpServer server;
+
+    public static void main(String[] args) throws IOException {
+
+        server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/convert", new EpochHandler());
+        server.start();
+        System.out.println("Service URL : http://localhost:8000/convert?epochTime=");
+
     }
 
-    public static String convertEpochToDateTime(long epochTime) {
+    public static LocalDateTime convertEpochToDateTime(long epochTime) {
         Instant instant = Instant.ofEpochSecond(epochTime);
-        LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneOffset.ofHoursMinutes(5,30));
-        return dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        return LocalDateTime.ofInstant(instant, ZoneOffset.ofHoursMinutes(5, 30));
+    }
+
+
+    static class EpochHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            Map<String, List<String>> params = parseQueryParams(exchange.getRequestURI().getQuery());
+
+            if (params.containsKey("epochTime")) {
+
+                long epochTime = Long.parseLong(params.get("epochTime").get(0));
+                var dateTime = convertEpochToDateTime(epochTime);
+                System.out.println("Converted provided epoch time : " + epochTime + " to Date Time Format : " + dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
+                String htmlResponse = "<!DOCTYPE html>\n" +
+                        "<html>\n" +
+                        "<head>\n" +
+                        "    <title>Epoch Time Converter</title>\n" +
+                        "    <style>\n" +
+                        "        body {\n" +
+                        "            font-family: Arial, sans-serif;\n" +
+                        "            background-color: #f4f4f4;\n" +
+                        "            margin: 0;\n" +
+                        "            padding: 0;\n" +
+                        "        }\n" +
+                        "        .container {\n" +
+                        "            max-width: 800px;\n" +
+                        "            margin: 50px auto;\n" +
+                        "            padding: 20px;\n" +
+                        "            background-color: #fff;\n" +
+                        "            border-radius: 10px;\n" +
+                        "            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);\n" +
+                        "            display: flex;\n" +
+                        "            justify-content: space-between;\n" +
+                        "        }\n" +
+                        "        .card {\n" +
+                        "            padding: 20px;\n" +
+                        "            background-color: #f9f9f9;\n" +
+                        "            border-radius: 5px;\n" +
+                        "            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);\n" +
+                        "            text-align: center;\n" +
+                        "        }\n" +
+                        "        h2 {\n" +
+                        "            color: #333;\n" +
+                        "        }\n" +
+                        "        p {\n" +
+                        "            color: #666;\n" +
+                        "        }\n" +
+                        "    </style>\n" +
+                        "</head>\n" +
+                        "<body>\n" +
+                        "    <div class=\"container\">\n" +
+                        "        <div class=\"card\">\n" +
+                        "            <h2>Year</h2>\n" +
+                        "            <p>" + dateTime.getYear() + "</p>\n" +
+                        "        </div>\n" +
+                        "        <div class=\"card\">\n" +
+                        "            <h2>Month</h2>\n" +
+                        "            <p>" + dateTime.getMonthValue() + "</p>\n" +
+                        "        </div>\n" +
+                        "        <div class=\"card\">\n" +
+                        "            <h2>Day</h2>\n" +
+                        "            <p>" + dateTime.getDayOfMonth() + "</p>\n" +
+                        "        </div>\n" +
+                        "        <div class=\"card\">\n" +
+                        "            <h2>Hours</h2>\n" +
+                        "            <p>" + dateTime.getHour() + "</p>\n" +
+                        "        </div>\n" +
+                        "        <div class=\"card\">\n" +
+                        "            <h2>Minutes</h2>\n" +
+                        "            <p>" + dateTime.getMinute() + "</p>\n" +
+                        "        </div>\n" +
+                        "        <div class=\"card\">\n" +
+                        "            <h2>Seconds</h2>\n" +
+                        "            <p>" + dateTime.getSecond() + "</p>\n" +
+                        "        </div>\n" +
+                        "    </div>\n" +
+                        "</body>\n" +
+                        "</html>";
+
+
+
+                exchange.getResponseHeaders().set("Content-Type", "text/html");
+                exchange.sendResponseHeaders(200, htmlResponse.getBytes().length);
+
+                try (OutputStream os = exchange.getResponseBody()) {
+                    os.write(htmlResponse.getBytes());
+                }
+            } else {
+
+                exchange.sendResponseHeaders(405, -1);
+            }
+        }
+
+        private Map<String, List<String>> parseQueryParams(String query) {
+            return Arrays
+                    .stream(query.split("&"))
+                    .map(p -> p.split("="))
+                    .collect(java.util.stream.Collectors.groupingBy(p -> p[0], java.util.stream.Collectors.mapping(p -> p[1], java.util.stream.Collectors.toList())));
+        }
+    }
+
+    public static void stopServer(){
+        server.stop(1);
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,25 +1,76 @@
 import org.bits.devops.Main;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MainTest {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @Test
     public void testConvertEpochToDateTime() {
         long epochTime = 1616634000; // Example epoch time
-        String expectedDateTime = "2021-03-25 06:30:00"; // Expected date and time
+        LocalDateTime expectedDateTime = LocalDateTime.parse("2021-03-25 06:30:00", formatter); // Expected date and time
 
-        String actualDateTime = Main.convertEpochToDateTime(epochTime);
-        assertEquals(expectedDateTime, actualDateTime);
+        LocalDateTime actualDateTime = Main.convertEpochToDateTime(epochTime);
+        assertEquals(expectedDateTime.getHour(), actualDateTime.getHour());
+        assertEquals(expectedDateTime.getMinute(), actualDateTime.getMinute());
+        assertEquals(expectedDateTime.getYear(), actualDateTime.getYear());
+        assertEquals(expectedDateTime.getMonth(), actualDateTime.getMonth());
     }
 
     @Test
     public void testConvertEpochToDateTimeNegative() {
         long epochTime = -1; // Negative epoch time
-        String expectedDateTime = "1970-01-01 05:29:59"; // Expected date and time for negative epoch time
+        LocalDateTime expectedDateTime = LocalDateTime.parse("1970-01-01 05:29:59", formatter); // Expected date and time for negative epoch time
 
-        String actualDateTime = Main.convertEpochToDateTime(epochTime);
-        assertEquals(expectedDateTime, actualDateTime);
+        LocalDateTime actualDateTime = Main.convertEpochToDateTime(epochTime);
+        assertEquals(expectedDateTime.getMonth(), actualDateTime.getMonth());
+        assertEquals(expectedDateTime.getHour(), actualDateTime.getHour());
+        assertEquals(expectedDateTime.getSecond(), actualDateTime.getSecond());
+    }
+
+    @Test
+    public void testEndpointWithResponse() throws URISyntaxException, IOException, InterruptedException {
+        Main.main(null);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("http://localhost:8000/convert?epochTime=1714417738"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(response.statusCode(), 200);
+        String resBody = new String(response.body().getBytes(StandardCharsets.UTF_8));
+        assertTrue(resBody.contains("""
+                   <div class="card">
+                            <h2>Minutes</h2>
+                            <p>38</p>
+                        </div>
+                """));
+        Main.stopServer();
+    }
+
+    @Test
+    public void testEndpointWithNoResponse() throws URISyntaxException, IOException, InterruptedException {
+        Main.main(null);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("http://localhost:8000/convert?epoch=1714417738"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(response.statusCode(), 405);
+
+        Main.stopServer();
     }
 }
